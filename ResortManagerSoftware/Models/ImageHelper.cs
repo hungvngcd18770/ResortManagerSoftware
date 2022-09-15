874882581c9e3bc8590a9847e62352ba650c4f1b
hungvngcd18770 <hungvngcd18770@fpt.edu.vn>
2022-09-15 17:44:04 +0700

    Add ImageHelper

---
+++ ResortManagerSoftware/Models/ImageHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResortManagerSoftware.Models
+{
+    public static class ImageHelper
+    {
+        public static MvcHtmlString Image(this HtmlHelper helper , string src , 
+            string alText, string height)
+        {
+            var builer = new TagBuilder("img");
+            builer.MergeAttribute("src", src);
+            builer.MergeAttribute("alt", alText);
+            builer.MergeAttribute("height", height);
+            return MvcHtmlString.Create(builer.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
