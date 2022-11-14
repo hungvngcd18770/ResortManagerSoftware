93ddbc56c005fdd7dea3cc71b56c4169dd23a767
hungvngcd18770 <hungvngcd18770@fpt.edu.vn>
2022-11-14 11:25:54 +0700

    Update Layout and fix LoginPartial

--- ResortManagerSoftware/Models/IdentityModels.cs
+++ ResortManagerSoftware/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace ResortManagerSoftware.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ResortManagerEntities", throwIfV1Schema: false)
         {
         }
 
